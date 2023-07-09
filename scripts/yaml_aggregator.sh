972b24516a5f2b2f1a2a5d8dcd27450d8dcdeabc
adv_gchatzopoulos <gchatzopoulos@advisable.gr>
2023-07-09 17:40:56 +0300

    Added help for yaml agg

--- scripts/yaml_aggregator.sh
+++ scripts/yaml_aggregator.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+if [ "$1" == "-h" ]
+then
+    echo "yaml_aggregator.sh: Aggregates all yaml files in a directory into a single yaml file and outputs to stdout."
+    echo "Usage: yaml_aggregator.sh <directory> [-f]"
+    echo "  -f: Prepend the file location to each yaml file as the root key."
+    echo "Example: yaml_aggregator.sh ./random_gitops_cluster_stuff -f > ./all.yaml"
+    exit 0
+fi
+
 if [ -z "$1" ]
 then
     echo "Please provide a directory."
